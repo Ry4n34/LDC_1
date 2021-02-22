4f3b675df1ef1e7167d0d0e31c63fdc607fd2c5d
Ry4n34 <79420611+Ry4n34@users.noreply.github.com>
2021-02-22 22:16:58 +0000

    Add project files.

---
+++ LDC/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+namespace LogicalPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+       
+        {
+
+ 
+            Console.Write("Enter a String : ");
+            string inputString = Console.ReadLine();
+            string resultString = string.Empty;
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (!resultString.Contains(inputString[i]))
+                {
+                    resultString += inputString[i];
+                }
+            }
+
+            if (inputString.Length > 15)
+            {
+                //shorten string
+                inputString = resultString.Substring(0, 15);
+            }
+            Console.WriteLine(resultString.Replace('$', '£').Replace('_', 'j').Replace('4', '5'));
+            Console.ReadKey();
+        }
+    }
+}
+
